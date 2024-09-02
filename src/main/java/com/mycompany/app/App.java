26c81f9de21e6db92c4eb036a85266a82e38e63b
vakkachan1991 <68135031+vakkachan1991@users.noreply.github.com>
2024-09-01 23:58:12 -0700

    Update App.java
    jojo

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,6 @@ package com.mycompany.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World from Aiswaryaaan and vakkkachaan and mollysusan!");
+        System.out.println("Hello World from Aiswaryaaan and vakkkachaan and mollysusan!and jojo");
     }
 }
