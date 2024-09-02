3f6f36867973d8ed7bfd5c02d701066f0d7ba8a4
vakkachan1991 <68135031+vakkachan1991@users.noreply.github.com>
2024-09-01 22:40:02 -0700

    Update App.java
    vakkachan added

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,6 @@ package com.mycompany.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World from Aiswaryaaan!");
+        System.out.println("Hello World from Aiswaryaaan and vakkkachaan!");
     }
 }
